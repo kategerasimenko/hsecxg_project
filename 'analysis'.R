d216ef1e38c19542f323753c3d52ff723992eaba
kategerasimenko <katgerasimenko@gmail.com>
2017-12-21 06:38:00 +0400

    Add files via upload

--- 'analysis'.R
+++ 'analysis'.R
@@ -1,6 +1,6 @@
 library(party)
 
-data <- read.csv(file="D:/data_plus_clust.csv", header=TRUE, sep=";")
+data <- read.csv(file="D:/Катя/Учеба/деплом/конструкции/data_plus_clust.csv", header=TRUE, sep=";")
 data <- data[,c('control.','answer','matrix_clust10_class','matrix_clust20_class',
                  'embedded_clust10_class','embedded_clust20_class')]
 colnames(data) <- c('c','answer','m10','m20','e10','e20')
@@ -10,8 +10,13 @@ data[,'e10']<- as.factor(data[,'e10'])
 data[,'e20']<- as.factor(data[,'e20'])
 data[,'c']<- as.factor(data[,'c'])
 
-endeavour <- glm(answer ~ m20 + e10,data,family='binomial' )
+endeavour <- glm(answer ~ m20+e10,data,family='binomial' )
 summary(endeavour)
+logit_pred = predict(endeavour,type='response')
 
 t <- ctree(answer ~ m20+e10,data=data)
 plot(t)
+tree_pred = predict(t,type='response')
+
+dd = data.frame(logit_pred,tree_pred,data[,'answer'])
+write.csv(dd,'D:/Катя/Учеба/деплом/конструкции/predictions.csv')
